88fca68d995da33ad5a4bce9135f1358653cd434
gavinarori <arorigavin@gmail.com>
2024-12-23 12:47:00 +0300

    added: certifications timeline

---
+++ src/components/ui/timeline.tsx
@@ -0,0 +1,82 @@
+"use client";
+import {
+  useMotionValueEvent,
+  useScroll,
+  useTransform,
+  motion,
+} from "framer-motion";
+import React, { useEffect, useRef, useState } from "react";
+
+interface TimelineEntry {
+  logoUrl: string | undefined;
+  title: string;
+  content: React.ReactNode;
+}
+
+export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [height, setHeight] = useState(0);
+
+  useEffect(() => {
+    if (ref.current) {
+      const rect = ref.current.getBoundingClientRect();
+      setHeight(rect.height);
+    }
+  }, [ref]);
+
+  const { scrollYProgress } = useScroll({
+    target: containerRef,
+    offset: ["start 10%", "end 50%"],
+  });
+
+  const heightTransform = useTransform(scrollYProgress, [0, 1], [0, height]);
+  const opacityTransform = useTransform(scrollYProgress, [0, 0.1], [0, 1]);
+
+  return (
+    <div
+      className="w-full bg-background font-sans "
+      ref={containerRef}
+    >
+      <div ref={ref} className="relative max-w-7xl mx-auto pb-20">
+        {data.map((item, index) => (
+          <div
+          key={index}
+          className="flex justify-start pt-20 md:flex-row flex-col gap-4" 
+        >
+          <div className="sticky flex items-center z-40 top-40 self-start max-w-xs lg:max-w-sm md:w-full">
+            {/* Logo */}
+            <div className="h-10 w-10 rounded-full overflow-hidden ml-3"> 
+              <img
+                src={item.logoUrl}
+                alt={`${item.title} logo`}
+                className="object-cover w-full h-full"
+              />
+            </div>
+        
+            {/* Content */}
+            <div className="flex flex-col ml-6">
+              {item.content}
+            </div>
+          </div>
+        </div>
+        
+        ))}
+        <div
+          style={{
+            height: height + "px",
+          }}
+          className="absolute md:left-8 left-8 top-0 overflow-hidden w-[2px] bg-[linear-gradient(to_bottom,var(--tw-gradient-stops))] from-transparent from-[0%] via-neutral-200 dark:via-neutral-700 to-transparent to-[99%]  [mask-image:linear-gradient(to_bottom,transparent_0%,black_10%,black_90%,transparent_100%)] "
+        >
+          <motion.div
+            style={{
+              height: heightTransform,
+              opacity: opacityTransform,
+            }}
+            className="absolute inset-x-0 top-0  w-[2px] bg-gradient-to-t from-purple-500 via-blue-500 to-transparent from-[0%] via-[10%] rounded-full"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
